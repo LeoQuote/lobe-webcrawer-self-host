ec2f29559c3dc50b5886c44fcaaec64061724de1
qizhicheng <qizhicheng@douban.com>
2024-03-10 11:39:56 +0800

    :zap: chore: delete runtime edge

--- src/pages/api/gateway.ts
+++ src/pages/api/gateway.ts
@@ -1,9 +1,5 @@
 import { createGatewayOnEdgeRuntime } from '@lobehub/chat-plugins-gateway';
 
-export const config = {
-  runtime: 'edge',
-};
-
 const handler = createGatewayOnEdgeRuntime();
 
 export default async (req: Request) => {
